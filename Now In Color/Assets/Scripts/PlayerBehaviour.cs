7fa2bc2feb0e7777cb8c4a4ac9c080efd714999d
Luke5150 <lhensley@mail.bradley.edu>
2021-03-02 10:53:39 -0600

    Fixed platform, added rigidbody components, started code for player movement

--- Now In Color/Assets/Scripts/PlayerBehaviour.cs
+++ Now In Color/Assets/Scripts/PlayerBehaviour.cs
@@ -7,15 +7,32 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public SpriteRenderer sr;
+    public Rigidbody2D rb2d;
+    public int speed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Player does things");
+        sr = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+    void movement()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+
+        if (xMove != 0)
+        {
+            sr.flipX = xMove < 0;
+        }
+
+        Vector2 moveForce = new Vector2(xMove * speed * Time.deltaTime, 0);
+        rb2d.AddForce(moveForce);
     }
 }
