f72c6e79df19a4bb7c613c02a1340e7083b3fadb
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2021-03-07 21:41:32 -0600

    Reset/Escape button functionality

--- Now In Color/Assets/Scripts/GameController.cs
+++ Now In Color/Assets/Scripts/GameController.cs
@@ -16,6 +16,13 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
     }
 }
