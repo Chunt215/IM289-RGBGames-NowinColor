40956efc8dcba2c5572f49e831d8c8cee222e170
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2021-03-07 21:36:49 -0600

    Revert "Added Reset/Quit Game buttons"
    This reverts commit 3b8fec4d4250d684d8861abc30bcf4f923cfd7b9.

--- Now In Color/Assets/Scripts/GameController.cs
+++ Now In Color/Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 /*
 spawning and platform behaviour stuff 
 */
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -14,13 +16,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-        else if (Input.GetKey(KeyCode.R))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-        }
+        
     }
 }
