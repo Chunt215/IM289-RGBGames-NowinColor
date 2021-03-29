e86e545b8d506a4af7c4a246397214ac12db275c
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2021-03-28 21:14:57 -0500

    Added Boss + adjusted levels and art

--- Now In Color/Assets/Scripts/DontDestroy.cs
+++ Now In Color/Assets/Scripts/DontDestroy.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
+    private Scene scene;
+
+    private GameObject playerObj;
+    private GameObject playerUI;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+
+        playerObj = GameObject.Find("Player");
+        playerUI = GameObject.Find("PlayerUI");
+    }
+
     private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (scene.name == "Start Screen")
+        {
+            Destroy(playerObj);
+            Destroy(playerUI);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 }
