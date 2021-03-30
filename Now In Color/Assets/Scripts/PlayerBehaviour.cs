6d69c0616e21f68a96096fb695dcd23d66da0c51
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2021-03-29 23:14:37 -0500

    Small Change for Presentation

--- Now In Color/Assets/Scripts/PlayerBehaviour.cs
+++ Now In Color/Assets/Scripts/PlayerBehaviour.cs
@@ -15,7 +15,7 @@ public class PlayerBehaviour : MonoBehaviour
     public Collider2D standingCollider;
     public Collider2D crouchingCollider;
 
-    static public int lives = 3;
+    static public int lives = 10;
     public LayerMask mask;
     public Text livesText;
     public int damage = 1;
