6d77b3dc2bffd2648853c7d48ba9047ff73ae653
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2021-04-04 21:51:26 -0500

    Removed unneeded code

--- Now In Color/Assets/Scripts/DontDestroy.cs
+++ Now In Color/Assets/Scripts/DontDestroy.cs
@@ -3,26 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
-    private Scene scene;
-
-    void Start()
-    {
-        scene = SceneManager.GetActiveScene();
-    }
-
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
-
-     /*   if (scene.buildIndex == 0)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-
-        }*/
-
 
         if (FindObjectsOfType<DontDestroy>().Length > 2)
         {
