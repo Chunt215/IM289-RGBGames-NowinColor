8e822c66283b76ce013d6f2eae6ff07bb22b9790
Chunt215 <60329298+Chunt215@users.noreply.github.com>
2021-04-13 10:29:27 -0500

    Level 3 Win Con

--- Now In Color/Assets/Scripts/GameController.cs
+++ Now In Color/Assets/Scripts/GameController.cs
@@ -129,15 +129,21 @@ public class GameController : MonoBehaviour
             case "Level 3":
                 for (int i = 0; i < platforms.Count; i++)
                 {
-                    SpriteRenderer platformSR = platforms[i].GetComponent<SpriteRenderer>();
-                    if (platformSR.color == Color.white)
+                    //SpriteRenderer platformSR = platforms[i].GetComponent<SpriteRenderer>();
+                    //if (platformSR.color == Color.white)
+                    //{
+                    //    colored = false;
+                    //    return colored;
+                    //}
+                    //else
+                    //{
+                    //    colored = true;
+                    //}
+                    colored = platforms[i].GetComponent<PlatformBehaviour>().correctColor;
+
+                    if (colored == false)
                     {
-                        colored = false;
                         return colored;
-                    }
-                    else
-                    {
-                        colored = true;
                     }
                 }
                 break;
